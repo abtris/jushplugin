e81355bc0e3849cabe9756af7f4508ca95f4b533
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-04-01 14:40:45 +0200

    First version

---
+++ jush.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: JUSH
+Plugin URI: http://jush.sourceforge.net/
+Description: JUSH Wordpress plugin
+Version: 1
+Author: Ladislav Prskavec
+Author URI: http://ladislav.prskavec.net
+*/
+
+if (!class_exists("JushPlugin")) {
+        class JushPlugin {					
+				function JushPlugin()
+				{
+					echo  '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/jush/jush.css" />' . "\n";
+				}
+				
+				function addHeaderCode() {
+                  echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/jushplugin/css/jush.css" />' . "\n";
+                  echo '<script type="text/javascript" src="' . get_bloginfo('wpurl') . '/wp-content/plugins/jushplugin/js/jush.js"></script>'. "\n";
+				  echo '<script type="text/javascript">'. "\n";
+				  echo "jush.style('jush.css');\n";
+				  echo "jush.highlight_tag('pre');\n";
+				  echo '</script>';
+                }
+				
+		}
+	}
+
+if (class_exists("JushPlugin")) {
+        $jush_plugin = new JushPlugin();
+}
+
+
+if (isset($jush_plugin)) {
+        //Actions    
+        add_action('wp_head', array(&$jush_plugin, 'addHeaderCode'), 1);
+}
